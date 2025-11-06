10d735150b5b2beae71d9fda76d0e0cc2297ff96
Omkesh <omkeshjadhav.dev@gmail.com>
2025-11-06 08:13:16 +0530

    Used useSelector hook to access the user data and display it in the Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Navbar = () => {
+    const user = useSelector((store) => store.user)
     return (
         <div className="navbar bg-base-100 shadow-sm">
             <div className="flex-1">
                 <a className="btn btn-ghost text-xl">DevTinder</a>
             </div>
-            <div className="flex gap-2">
+            {user && <div className="flex gap-2">
+                <div className='flex items-center'>Welcome {user.firstName}</div>
                 <button className="btn btn-ghost btn-circle">
                     <div className="indicator">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /> </svg>
@@ -17,8 +19,8 @@ const Navbar = () => {
                     <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
                         <div className="w-10 rounded-full">
                             <img
-                                alt="Tailwind CSS Navbar component"
-                                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                                alt="User Image"
+                                src={user.image} />
                         </div>
                     </div>
                     <ul
@@ -34,7 +36,7 @@ const Navbar = () => {
                         <li><a>Logout</a></li>
                     </ul>
                 </div>
-            </div>
+            </div>}
         </div>
     )
 }
